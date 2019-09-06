e6bee8823e1bdbae7b1f2ad4a97b1b435c70e6ea
georged <6376745+dodders@users.noreply.github.com>
2019-09-06 09:53:17 -0400

    day02

--- src/days/day02.clj
+++ src/days/day02.clj
@@ -2,8 +2,8 @@
   (:require [clojure.string :as str]))
 
 (defn get-inp []
-  (str/split (slurp "resources/data02.test.txt") #"\n"))
-  ;(str/split (slurp "resources/data02.txt") #"\n"))
+  ;(str/split (slurp "resources/data02.test.txt") #"\n"))
+  (str/split (slurp "resources/data02.txt") #"\n"))
 
 (defn same [x] x)
 
@@ -30,14 +30,16 @@
     ; of three counts, so answer is x * y...
     (reduce * total)))
 
-(defn diff-by-one? [s1 s2 diff]
+; true if the two words are different by one character.
+(defn diff-by-one? [w1 w2 diff]
   (cond
     (> diff 1) false
-    (and (= 0 (count s1)) (= diff 0)) false
-    (and (= 0 (count s1)) (= diff 1)) true
-    :else (diff-by-one? (rest s1) (rest s2)
-                        (if (= (first s1) (first s2)) diff (inc diff)))))
+    (and (empty? w1) (= diff 0)) false
+    (and (empty? w2) (= diff 1)) true
+    :else (diff-by-one? (rest w1) (rest w2)
+                        (if (= (first w1) (first w2)) diff (inc diff)))))
 
+; return only the common letters between words
 (defn get-common-letters [w1 w2 ret]
   (cond
     (= 0 (count w1)) ret
@@ -45,14 +47,28 @@
     (get-common-letters (rest w1) (rest w2) (conj ret (first w1)))
     :else  (get-common-letters (rest w1) (rest w2) ret)))
 
-; return a function that finds a match for word in words
-(defn get-match-fn [words]
-  (fn [word]
-    (prn word words)))
+; find a match for word in words
+(defn find-match [word words]
+  (cond
+    (empty? words) false
+    (diff-by-one? word (first words) 0) (list word (first words))
+    :else (find-match word (rest words))))
 
+; create closure of words so we can use find-match as a filter
+(defn get-filter [words]
+  (fn [word]
+    (find-match word words)))
+
+; find all matches in word list (should reduce to a single pair of words)
+(defn get-matches []
+  (let [inp (get-inp)
+        match-filter (get-filter inp)]
+    ;reduce list of words to a list of matching pairs
+    (filter match-filter inp)))
 
 (defn part2 []
-  (let [inp (get-inp)
-        match (get-match-fn inp)]
-    (map match inp)))
+  (let [matches (get-matches)]
+    (reduce str (get-common-letters (first matches) (second matches) []))))
+
+
 
